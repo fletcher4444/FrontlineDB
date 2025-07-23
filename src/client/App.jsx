8b056fab05721aad7ec98502c62f576b20013086
fletcher4444 <burtonfletcher87@gmail.com>
2025-07-23 15:26:41 +0000

    third-party

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -18,21 +18,16 @@
 // export default App;
 
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import LoginButton from "./components/auth/LoginButton";
-import LogoutButton from "./components/auth/LogoutButton";
-import Profile from "./components/profile/Profile";
-import Layout from "./components/navbar/Layout";
 
+import Layout from "./components/navbar/Layout";
+import AircraftList from "./pages/AircraftList";
 const App = () => {
-  const { isAuthenticated } = useAuth0();
+
 
   return (
     <div className="app">
         <Layout />
-      <h1>Welcome to FrontlineDB</h1>
-      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
-      <Profile />
+       <AircraftList />
     </div>
   );
 };
